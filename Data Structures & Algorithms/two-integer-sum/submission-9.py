122c01a4017deaba36814ea7633ae53fddd301c8
Odie Yang <135147367+Odiethebest@users.noreply.github.com>
2026-06-27 18:32:33 -0700

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mySet = defaultdict()
+        for i, n in enumerate(nums):
+            comp = target - n
+            if comp in mySet:
+                    return [mySet[comp],i]
+            mySet[n] = i
+        return -1
