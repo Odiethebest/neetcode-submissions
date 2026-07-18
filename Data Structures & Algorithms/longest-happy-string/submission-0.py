2f2d6ba1a24535db63123250508b5d2f715c7ac4
Odie Yang <135147367+Odiethebest@users.noreply.github.com>
2026-07-17 18:12:46 -0700

    Add: longest-happy-string - submission-0

---
+++ Data Structures & Algorithms/longest-happy-string/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        cnt = [a, b, c]
+        res = []
+
+        def getMax(repeated):
+            idx = -1
+            maxCnt = 0
+            for i in range(3):
+                if i == repeated or cnt [i] == 0:
+                    continue
+                
+                if maxCnt < cnt[i]:
+                    maxCnt = cnt[i]
+                    idx = i
+            return idx
+        repeated = -1
+        while True:
+            maxChar = getMax(repeated)
+            if maxChar == -1: break
+            res.append(chr(maxChar + ord('a')))
+            cnt[maxChar] -= 1
+            if len(res) > 1 and res[-1] == res[-2]:
+                repeated = maxChar
+            else:
+                repeated = -1
+        
+        return ''.join(res)
+
+
+
+
+
