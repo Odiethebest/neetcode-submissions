a0cd18f60208d3b8c0c045f0a9f1216a20d9bce0
Odie Yang <135147367+Odiethebest@users.noreply.github.com>
2026-07-26 17:02:14 -0700

    Add: concatenated-words - submission-1

---
+++ Data Structures & Algorithms/concatenated-words/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        wordSet = set(words)
+        memo = {}
+
+        def dfs(word):
+            if word in memo:
+                return memo[word]
+            for i in range(1, len(word)):
+                prefix, suffix = word[:i], word[i:]
+                if (prefix in wordSet and (suffix in wordSet or dfs(suffix))):
+                    memo[word] = True
+                    return True
+            memo[word] = False
+            return False
+        
+        res = []
+        for w in words:
+            if dfs(w):
+                res.append(w)
+        return res
