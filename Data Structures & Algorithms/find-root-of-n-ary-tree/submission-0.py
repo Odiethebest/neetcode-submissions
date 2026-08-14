b69a88870c6bc549c4fac0a01da8621fe95f1c6f
Odie Yang <135147367+Odiethebest@users.noreply.github.com>
2026-08-14 12:07:07 -0700

    Add: find-root-of-n-ary-tree - submission-0

---
+++ Data Structures & Algorithms/find-root-of-n-ary-tree/submission-0.py
@@ -0,0 +1,39 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def findRoot(self, tree: List['Node']) -> 'Node':
+        value_sum = 0
+
+        for node in tree:
+            value_sum += node.val
+            for child in node.children:
+                value_sum -= child.val
+        
+
+        for node in tree:
+            if node.val == value_sum:
+                return node
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
